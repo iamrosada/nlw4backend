c52d30d8c967013da236e0192c4c1af9c97daa3d
77-aguarosada <luisrosada2018@gmail.com>
2021-02-22 21:06:48 -0300

    start api

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+
+
+app.get("/",(request, response)=>{
+   
+    return response.json({mensage:"hello world"});
+})
+
+app.listen(3333,()=>{
+    console.log("server is runing");
+});
