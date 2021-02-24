2ea162dba1df704b722f26e3e6c415d885358db8
77-aguarosada <luisrosada2018@gmail.com>
2021-02-24 19:26:40 -0300

    added database

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import express from "express";
 
 const app = express();
 
-
-
 app.get("/",(request, response)=>{
    
     return response.json({mensage:"hello world"});
+})
+
+app.post("/",(request,response)=>{
+    return response.json({mensage:"os dados foram salvos"})
 })
 
 app.listen(3333,()=>{
