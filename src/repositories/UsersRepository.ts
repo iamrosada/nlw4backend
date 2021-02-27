d741fd520a877a5522bd86893a03d6c19e4eb755
77-aguarosada <luisrosada2018@gmail.com>
2021-02-27 16:32:14 -0300

    update repositories

---
+++ src/repositories/UsersRepository.ts
@@ -0,0 +1,12 @@
+import { EntityRepository, Repository } from "typeorm";
+import { User } from "../models/User";
+
+
+@EntityRepository(User)
+class UserRespository extends Repository<User>{
+
+
+
+}
+
+export {UserRespository}
