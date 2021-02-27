717a12a040bb1e23bdeca1bff8329a87ee13c2e9
77-aguarosada <luisrosada2018@gmail.com>
2021-02-27 16:54:17 -0300

    added controller de pesquisa

---
+++ src/controllers/SurveysController.ts
@@ -0,0 +1,11 @@
+import { Response, Request } from "express";
+
+class SurveyController{
+    async create(request :Request , response:Response){
+            
+    }
+
+
+}
+
+export {SurveyController}
