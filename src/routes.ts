bf2eca6645b9f8850e363962b844c417944e299f
77-aguarosada <luisrosada2018@gmail.com>
2021-02-27 17:19:25 -0300

    creted router for survey

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import {Router} from 'express';
+import { SurveyController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
 
 
 const router = Router();
 
 const  userController = new UserController()
+const surveyController = new SurveyController()
 
 router.post("/users",userController.create)
+router.post("/survey",surveyController.create)
 
 export {router}
