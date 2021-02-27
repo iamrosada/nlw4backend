4e94ce0bb4ecba29fd6d8679f0eb167eaf9509b2
77-aguarosada <luisrosada2018@gmail.com>
2021-02-27 17:18:05 -0300

    update

--- src/controllers/SurveysController.ts
+++ src/controllers/SurveysController.ts
@@ -1,8 +1,23 @@
 import { Response, Request } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysRepository } from "../repositories/SurveysRepository";
 
 class SurveyController{
     async create(request :Request , response:Response){
-            
+        const {title, description} = request.body;
+
+        const surveyRepository = getCustomRepository(SurveysRepository);
+
+        //realizou a pesquisa 
+        const servey = surveyRepository.create({
+            title,
+            description                                             
+        })
+        //guardar os dados da pesquisa
+        await surveyRepository.save(servey);
+
+        //retornar a pesquisa que ele criou 
+        return response.status(201).json(servey);
     }
 
 
